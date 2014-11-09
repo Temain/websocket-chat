3775bc9266ce986d345eb733c029466405773359
Temain <temain@mail.ru>
2014-11-09 05:08:29 +0400

    websocketconnection

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function(){
 
-    var dispatcher = new WebSocketRails('temain.tk:3001/websocket');
+    var dispatcher = new WebSocketRails('temain.tk:3000/websocket');
     // var dispatcher = new WebSocketRails('0.0.0.0:3000/websocket');
 
     var chat = {
